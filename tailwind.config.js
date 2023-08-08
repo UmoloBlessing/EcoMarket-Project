c46a7b4e922eb82ed014fea66b10246d8e1c305b
Umolo Blessing Johnson <umoloblessing009@gmail.com>
2023-08-08 03:08:50 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'header': '#007D6E',
+        'background': '#EEF7F2',
+        'footer': '#007D6E',
+        'text-2': '#272727',
+        'header-bg': '#1A1A1A66'
+      },
+      fontFamily: {
+        'nav': ['inter', 'sans-serif'],
+        'header-1': ['Playfair Display', 'sans-serif'],
+        'header-2': ['Open Sans', 'sans-serif'],
+        'header-3': ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
